386445290ab7405c0a8ebca1f7af3ca0e9efecf6
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-08-30 09:45:44 +0200

    [IMP] sale_order_line_planned_quantity: computation similar to delivered

--- sale_order_line_planned_quantity/__manifest__.py
+++ sale_order_line_planned_quantity/__manifest__.py
@@ -3,7 +3,11 @@
 {
     "name": "Sale Order Line Planned Quantity",
     "version": "14.0.1.0.0",
-    "depends": ["sale", "stock", "sale_stock"],
+    "depends": [
+        "sale",
+        "stock",
+        "sale_stock",
+    ],
     "author": "AvanzOSC",
     "license": "AGPL-3",
     "website": "https://github.com/avanzosc/sale-addons",
--- sale_order_line_planned_quantity/models/sale_order_line.py
+++ sale_order_line_planned_quantity/models/sale_order_line.py
@@ -17,50 +17,79 @@ class SaleOrderLine(models.Model):
         string="Difference Between Ordered And Planned",
         store=True,
         copy=False,
-        compute="_compute_difference_between_ordered_planned",
+        compute="_compute_planned_quantity",
     )
 
-    @api.depends(
-        "product_uom_qty",
-        "state",
-        "move_ids",
-        "move_ids.state",
-        "move_ids.product_uom_qty",
-        "move_ids.picking_type_id",
-        "move_ids.picking_type_id.use_to_calculate_planned_quantities",
-    )
+    @api.depends('move_ids.state', 'move_ids.scrapped', 'move_ids.product_uom_qty', 'move_ids.product_uom')
     def _compute_planned_quantity(self):
-        for line in self:
-            planned_quantity = 0
-            moves_planned_quantities = False
-            if line.move_ids:
-                moves_planned_quantities = line.move_ids.filtered(
-                    lambda x: x.picking_type_id.use_to_calculate_planned_quantities
-                )
-                if moves_planned_quantities:
-                    moves = moves_planned_quantities.filtered(
-                        lambda x: x.state != "cancel"
-                    )
-                    if moves:
-                        planned_quantity = sum(moves.mapped("product_uom_qty"))
-            line.planned_quantity = planned_quantity if moves_planned_quantities else 0
-
-    @api.depends(
-        "planned_quantity",
-        "state",
-        "product_uom_qty",
-    )
-    def _compute_difference_between_ordered_planned(self):
-        for line in self:
-            moves_planned_quantities = False
-            if line.move_ids:
-                moves_planned_quantities = line.move_ids.filtered(
-                    lambda x: x.picking_type_id.use_to_calculate_planned_quantities
-                )
-            if moves_planned_quantities:
-                line.difference_between_ordered_planned = bool(
-                    line.product_uom_qty != line.planned_quantity
-                    and line.state != "draft"
-                )
+        for line in self:  # TODO: maybe one day, this should be done in SQL for performance sake
+            if line.qty_delivered_method == 'stock_move':
+                qty = 0.0
+                outgoing_moves, incoming_moves = line._get_outgoing_incoming_moves()
+                for move in outgoing_moves:
+                    if move.state in ('done', 'cancel'):
+                        continue
+                    qty += move.product_uom._compute_quantity(
+                        move.product_uom_qty, line.product_uom, rounding_method='HALF-UP')
+                for move in incoming_moves:
+                    if move.state in ('done', 'cancel'):
+                        continue
+                    qty -= move.product_uom._compute_quantity(
+                        move.product_uom_qty, line.product_uom, rounding_method='HALF-UP')
+                line.update({
+                    'planned_quantity': qty,
+                    'difference_between_ordered_planned': bool(line.product_uom_qty != qty),
+                })
             else:
-                line.difference_between_ordered_planned = False
+                line.update({
+                    'planned_quantity': line.product_uom_qty,
+                    'difference_between_ordered_planned': False,
+                })
+
+    # @api.depends(
+    #     "product_uom_qty",
+    #     "move_ids",
+    #     "move_ids.state",
+    #     "move_ids.product_uom_qty",
+    #     "move_ids.picking_type_id",
+    #     "move_ids.picking_type_id.use_to_calculate_planned_quantities",
+    # )
+    # def _compute_planned_quantity(self):
+    #     for line in self:
+    #         planned_quantity = 0
+    #         moves_planned_quantities = False
+    #         if line.qty_delivered_method == "stock_move":
+    #             moves = line.move_ids.filtered(
+    #                 lambda m: m.state not in ("done", "cancel") and not m.scrapped
+    #             )
+    #             if line.move_ids:
+    #                 moves_planned_quantities = line.move_ids.filtered(
+    #                     lambda x: x.picking_type_id.use_to_calculate_planned_quantities
+    #                 )
+    #                 if moves_planned_quantities:
+    #                     moves = moves_planned_quantities.filtered(
+    #                         lambda x: x.state != "cancel"
+    #                     )
+    #                     if moves:
+    #                         planned_quantity = sum(moves.mapped("product_uom_qty"))
+    #         line.planned_quantity = planned_quantity if moves_planned_quantities else 0
+
+    # @api.depends(
+    #     "planned_quantity",
+    #     "state",
+    #     "product_uom_qty",
+    # )
+    # def _compute_difference_between_ordered_planned(self):
+    #     for line in self:
+    #         moves_planned_quantities = False
+    #         if line.move_ids:
+    #             moves_planned_quantities = line.move_ids.filtered(
+    #                 lambda x: x.picking_type_id.use_to_calculate_planned_quantities
+    #             )
+    #         if moves_planned_quantities:
+    #             line.difference_between_ordered_planned = bool(
+    #                 line.product_uom_qty != line.planned_quantity
+    #                 and line.state != "draft"
+    #             )
+    #         else:
+    #             line.difference_between_ordered_planned = False
